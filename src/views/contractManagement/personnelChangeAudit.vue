7d2fab00eaf9a3b460c1899ef4c8efdb977ad28b
‘王海林’ <2399827738@qq.com>
2022-05-16 18:06:45 +0800

    复用样式

--- src/views/contractManagement/personnelChangeAudit.vue
+++ src/views/contractManagement/personnelChangeAudit.vue
@@ -9,189 +9,259 @@
 <template>
   <el-container class="container-box">
     <el-header>
-      <!--      <el-button type="primary" size="medium" @click="openDialog">新增</el-button>-->
+      <div class="input-box">
+        <div class="input-value">
+          <el-input v-model="queryData.projectCode" placeholder="请输入标段"></el-input>
+        </div>
+
+      </div>
+      <div class="input-box">
+        <div class="input-value">
+          <el-input v-model="queryData.subProject" placeholder="请输入负责人"></el-input>
+        </div>
+      </div>
+      <el-button type="primary">搜索</el-button>
+
+      <div class="right-btns">
+        <!-- <el-button type="primary" size="small"
+          :icon="operateBtnsVisible?'el-icon-d-arrow-right':'el-icon-d-arrow-left'"
+          @click="operateBtnsVisible=!operateBtnsVisible"></el-button> -->
+        <div class="operate-btns">
+          <!--        <el-button size="small" @click="openDialog">新增填报</el-button>-->
+          <el-button size="small">导出</el-button>
+          <el-button size="small">批量操作</el-button>
+        </div>
+      </div>
     </el-header>
     <el-main>
-      <el-table :data="tableDta" style="width: 100%" border height="100%">
-        <el-table-column prop="uploadname" label="标段"></el-table-column>
-        <el-table-column prop="uploadname" label="人员变更类型"></el-table-column>
-        <el-table-column prop="uploadname" label="变更岗位"></el-table-column>
-        <el-table-column prop="uploadname" label="变更前人员"></el-table-column>
-        <el-table-column prop="uploadname" label="变更后人员"></el-table-column>
-        <el-table-column prop="uploadname" label="录入日期"></el-table-column>
-        <el-table-column prop="uploadname" label="负责人"></el-table-column>
-        <el-table-column prop="uploadname" label="操作"></el-table-column>
-      </el-table>
+      <div class="container">
+        <el-table :data="tableDta" style="width: 100%" border height="calc(100% - 48px)" class="have_scrolling">
+          <el-table-column prop="uploadname" label="标段"></el-table-column>
+          <el-table-column prop="uploadname" label="人员变更类型"></el-table-column>
+          <el-table-column prop="uploadname" label="变更岗位"></el-table-column>
+          <el-table-column prop="uploadname" label="变更前人员"></el-table-column>
+          <el-table-column prop="uploadname" label="变更后人员"></el-table-column>
+          <el-table-column prop="uploadname" label="录入日期"></el-table-column>
+          <el-table-column prop="uploadname" label="负责人"></el-table-column>
+          <el-table-column prop="uploadname" label="操作"></el-table-column>
+        </el-table>
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                       :current-page="queryData.pageNum" :page-size="queryData.pageSize"
+                       layout="total, sizes, prev, pager, next, jumper"
+                       :total="queryData.totalPage">
+        </el-pagination>
+      </div>
+
     </el-main>
-    <el-footer></el-footer>
-    <el-dialog class="full_global_dialog" title="详情" :fullscreen="true" :visible.sync="dialogFormVisible">
+    <el-dialog class="full-dialog defined-dialog" :fullscreen="true" :visible.sync="dialogFormVisible">
+      <template slot="title">
+        {{ dialogTitle }}
+      </template>
       <el-container class="full_dialog_container">
-        <el-main>
-          <div class="form_content">
-            <el-form ref="form" label-width="120px">
-              <div class="header_title">人员变更审核</div>
-              <div class="form_item">
-                <div class="form_item_title">
-                  <div class="title_bar"></div>
+        <el-main style="height: calc(100vh - 96px); overflow-y: scroll;padding: 0px;margin: 0;">
+          <div class="form-bg">
+            <el-form ref="form" :model="form" label-width="80px">
+              <div class="form-title">
+                <div class="title-big-bar"></div>
+                <strong>人员变更审核</strong>
+              </div>
+              <div class="form-block">
+                <div class="form-block-title">
+                  <div class="title-bar"></div>
                   <strong>基本信息</strong>
                 </div>
-                <div class="form_item_content">
-                  <el-row>
-                    <el-col :span="8">
-                      <el-form-item label="标段">
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                      <el-form-item label="填报人">
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="8">
-                      <el-form-item label="填报日期">
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">标段</div>
+                    <div class="block-item-value">
+                      <el-input readonly></el-input>
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">填报人</div>
+                    <div class="block-item-value">
+                      <el-input readonly></el-input>
+                    </div>
+                  </div>
                 </div>
-
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">填报日期</div>
+                    <div class="block-item-value">
+                      <el-date-picker
+                        v-model="form.time"
+                        type="datetime"
+                        :clearable="false"
+                        placeholder="选择日期时间"
+                        value-format="yyyy-MM-dd HH:mm:ss"
+                      >
+                      </el-date-picker>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div class="form_item">
-                <div class="form_item_title">
-                  <div class="title_bar"></div>
+              <div class="form-block">
+                <div class="form-block-title">
+                  <div class="title-bar"></div>
                   <strong>变更信息</strong>
                 </div>
-                <div class="form_item_content">
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="项目">
-                        <el-input></el-input>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">项目</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-input readonly></el-input>
                       </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="标段">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="人员变更类型">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="建设单位">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="变更单位">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="变更岗位">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="变更前人员">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="变更前(执业)资格证书号">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="变更后人员">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="变更后(执业)资格证书号">
-                        <el-input></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="变更原因">
-                        <el-input type="textarea"></el-input>
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <el-row :gutter="6">
-                    <el-col :span="24">
-                      <el-form-item label="附件">
-                      </el-form-item>
-                    </el-col>
-                  </el-row>
-                  <div class="form_item_table">
-                    <el-table :data="fileData" border style="width: 100%;">
-                      <el-table-column prop="uploadname" label="附件"></el-table-column>
-                      <el-table-column prop="uploadname" label="上传日期"></el-table-column>
-                      <el-table-column prop="uploadname" label="上传人"></el-table-column>
-                      <el-table-column prop="uploadname" label="操作"></el-table-column>
-                    </el-table>
+                    </div>
                   </div>
-                  <div class="word-wrap">
-                    <p>
-                      <span style="font-family: FangSong, FangSong_GB2312; font-size: 14px;">
-                        <em>应附：替换人与被替换人的简历表、职称证书、学历证书、身份证复印件、工作简历、业绩、社保、资格证书等相关材料。 </em>
-                      </span>
-                    </p>
-
+                  <div class="block-item">
+                    <div class="block-item-label">标段</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-input readonly></el-input>
+                      </el-form-item>
+                    </div>
                   </div>
                 </div>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">人员变更类型</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-select placeholder="请选择">
+                        </el-select>
+                      </el-form-item>
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">建设单位</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-select placeholder="请选择">
+                        </el-select>
+                      </el-form-item>
+                    </div>
+                  </div>
+                </div>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">变更单位</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-input readonly></el-input>
+                      </el-form-item>
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">变更岗位</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-select placeholder="请选择">
+                        </el-select>
+                      </el-form-item>
+                    </div>
+                  </div>
+                </div>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">变更前人员</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-select placeholder="请选择">
+                        </el-select>
+                      </el-form-item>
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">变更前(执业)资格证书号</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-input readonly></el-input>
+                      </el-form-item>
+                    </div>
+                  </div>
+                </div>
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">变更后人员</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-select placeholder="请选择">
+                        </el-select>
+                      </el-form-item>
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">变更后(执业)资格证书号</div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+                        <el-input readonly></el-input>
+                      </el-form-item>
+                    </div>
+                  </div>
+                </div>
+
+
               </div>
-              <div class="form_item">
-                <div class="form_item_title">
-                  <div class="title_bar"></div>
+              <div class="form-block">
+                <div class="form-block-title">
+                  <div class="title-bar"></div>
+                  <strong>附件</strong>
+                </div>
+                <div class="block-line">
+                  <el-button size="small" type="primary">下载全部</el-button>
+                </div>
+                <div class="block-table">
+                  <el-table :data="fileData" border style="width: 100%;">-->
+                    <el-table-column prop="uploadname" label="附件"></el-table-column>
+                    <el-table-column prop="uploadname" label="上传日期"></el-table-column>
+                    <el-table-column prop="uploadname" label="上传人"></el-table-column>
+                    <el-table-column prop="uploadname" label="操作"></el-table-column>
+                  </el-table>
+                </div>
+              </div>
+              <div class="form-block">
+                <div class="form-block-title">
+                  <div class="title-bar"></div>
                   <strong>待处理人</strong>
                 </div>
-                <div class="form_item_content">
-                  <el-row :gutter="6">
-                    <el-col :span="12">
-                      <el-form-item label="负责人">
+                <div class="block-line">
+                  <div class="block-item">
+                    <div class="block-item-label">负责人<i class="require-icon"></i></div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+
                       </el-form-item>
-                    </el-col>
-                    <el-col :span="12">
-                      <el-form-item label="项目部">
+                    </div>
+                  </div>
+                  <div class="block-item">
+                    <div class="block-item-label">项目部<i class="require-icon"></i></div>
+                    <div class="block-item-value">
+                      <el-form-item prop="qualityCheckUser">
+
                       </el-form-item>
-                    </el-col>
-                  </el-row>
-                </div>
-              </div>
-              <div class="form_item">
-                <div class="form_item_title">
-                  <div class="title_bar"></div>
-                  <strong>审核记录</strong>
-                </div>
-                <div class="form_item_content">
-                  <div class="form_item_table">
-                    <el-table :data="fileData" border style="width: 100%;">
-                      <el-table-column prop="uploadname" label="填报人"></el-table-column>
-                      <el-table-column prop="uploadname" label="审核人"></el-table-column>
-                      <el-table-column prop="uploadname" label="审核状态"></el-table-column>
-                      <el-table-column prop="uploadname" label="备注"></el-table-column>
-                    </el-table>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div class="form_item" style="text-align: center;padding: 5px 0">
-                <el-button type="primary">提交</el-button>
+              <div class="form-block">
+                <el-button class="submit-btn" size="small" type="primary">提交
+                </el-button>
               </div>
             </el-form>
-
           </div>
         </el-main>
+        <el-aside width="8px" class="close-wrapper">
+          <div class="close-wrap">
+            <i class="el-icon-caret-right"></i>
+          </div>
+        </el-aside>
+        <el-aside
+          style="width: 410px;background-color: rgb(242, 242, 242);overflow: scroll;height: calc(100vh - 96px);">
+          <!--              <tasklog></tasklog>-->
+        </el-aside>
       </el-container>
+
     </el-dialog>
   </el-container>
 </template>
@@ -203,7 +273,15 @@
         tableDta: [],
         fileData: [],//上传的附件
         form: {},
-        dialogFormVisible: true
+        dialogFormVisible: true,
+        dialogTitle: "项目全生命周期数字管理平台",
+        queryData: {
+          projectCode: "",
+          subProject: "",
+          pageNum: 1,
+          totalPage: 1,
+          pageSize: 10
+        }
       };
     },
     created() {
@@ -211,12 +289,31 @@
     components: {},
     computed: {},
     methods: {
-      openDialog() {
-        this.dialogFormVisible = true;
+      handleSizeChange() {
+      },
+      handleCurrentChange() {
       }
     }
   };
 </script>
 <style lang='scss' scoped>
+  @import "../../assets/css/table.scss";
+  @import "../../assets/css/dialog.scss";
+  .form-bg {
+    .form-block {
+      .block-item-label {
+        width: 180px !important;
+      }
+
+      .block-item-value {
+        width: calc(100% - 200px) !important;
+      }
+
+      .el-date-editor {
+        width: 100% !important;
+      }
+    }
+  }
+
 
 </style>
