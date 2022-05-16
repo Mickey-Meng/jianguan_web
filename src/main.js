28debbd98501e966ad6c7872ce4c4fb59d40030b
zhouhai <850168627@qq.com>
2022-05-16 22:29:08 +0800

    流程图展示
    流程图展示

--- src/main.js
+++ src/main.js
@@ -28,6 +28,8 @@ import "@/utils/geturl";
 
 import "@/assets/css/common.css"
 
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
 
 /**
  * If you don't want to use mock-server
