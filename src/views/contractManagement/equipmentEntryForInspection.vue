23dc7dc1bf29a2aba5c4348668bf8ee864c96597
zhouhai <850168627@qq.com>
2022-05-27 17:18:12 +0800

    设备进退场报验
    设备进退场报验

--- src/views/contractManagement/equipmentEntryForInspection.vue
+++ src/views/contractManagement/equipmentEntryForInspection.vue
@@ -7,30 +7,170 @@
 @modifierTime:
 -->
 <template>
-  <div>
+	<el-container class="container-box">
+		<el-header>
+			<div class="input-box">
+				<div class="input-value">
+					<el-input v-model="queryData.projectCode" placeholder="监理单位"></el-input>
+				</div>
 
-  </div>
+			</div>
+			<div class="input-box">
+				<div class="input-value">
+					<el-input v-model="queryData.subProject" placeholder="施工单位"></el-input>
+				</div>
+			</div>
+			<el-button type="primary">搜索</el-button>
+
+			<div class="right-btns">
+				<!-- <el-button type="primary" size="small"
+					:icon="operateBtnsVisible?'el-icon-d-arrow-right':'el-icon-d-arrow-left'"
+					@click="operateBtnsVisible=!operateBtnsVisible"></el-button> -->
+				<div class="operate-btns" v-show="operateBtnsVisible">
+					<el-button size="small" @click="addNew">新增</el-button>
+					<el-button size="small">导出</el-button>
+					<el-button size="small">批量操作</el-button>
+				</div>
+			</div>
+		</el-header>
+		<el-main>
+			<div class="container">
+				<el-table :data="tableData" style="width: 100%" border height="calc(100% - 48px)"
+					class="have_scrolling">
+					<el-table-column type="index" width="50" align="center" label="序号">
+					</el-table-column>
+					<el-table-column prop="projectName" align="center" label="工程名称" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="buildUnit" align="center" label="工程编号" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="contractCode" align="center" label="施工单位" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="supervisorUnit" align="center" label="合同段" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="buildSectionId" align="center" label="监理标段" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="supervisorSection" align="center" label="监理单位">
+					</el-table-column>
+					<el-table-column prop="supervisorSection" align="center" label="合同号">
+					</el-table-column>
+					<el-table-column prop="statusStr" align="center" label="状态描述">
+					</el-table-column>
+					<el-table-column fixed="right" width="120" align="center" label="操作">
+						<template slot-scope="{ row, $index }">
+							<el-button type="text" size="mini" @click="modify(row)">修改</el-button>
+							<el-button type="text" size="mini" @click="viewDetail(row)">详情</el-button>
+							<el-button type="text" size="mini" @click="deleteRow(row)">删除</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+					:current-page="queryData.pageNum" :page-size="queryData.pageSize" layout="total, sizes, prev, pager, next, jumper"
+					:total="queryData.totalPage">
+				</el-pagination>
+			</div>
+		</el-main>
+		<edit ref="edit" @query="query" :editRow="editRow"></edit>
+		<detail ref="detail" :detailRow="detailRow"></detail>
+	</el-container>
 </template>
 
 <script>
-  export default {
-    props: [],
-    watch: {},
-    data() {
-      return {};
-    },
-    created() {
-    },
-    mounted() {
-    },
-    methods: {},
-    components: {},
-    beforeDestroy() {
-    }
-
-  };
+	import * as api from "@/api/quality";
+	import edit from './equipmentEntryForInspection/edit';
+	import detail from './equipmentEntryForInspection/detail';
+	export default {
+		data() {
+			return {
+				allData:{},
+				tableData: [],
+				operateBtnsVisible: true,
+				dialogTitle: '智慧建设通用版-【绍兴市】235国道杭州',
+				dialogFormVisible: false,
+				queryData: {
+					projectCode: '',
+					subProject: '',
+					pageNum: 1,
+					totalPage: 1,
+					pageSize: 10,
+					projectId:this.$store.getters.project['id']
+				},
+				editRow:null,
+				detailRow:null
+			};
+		},
+		created() {},
+		components: {
+			edit,
+			detail
+		},
+		computed: {},
+		mounted() {
+			this.query();
+		},
+		methods: {
+			query() {
+				api.getHiddenProjectList(this.queryData).then((res) => {
+					this.allData = res.data || {};
+					this.tableData = this.formateTableData(res.data.list);
+					this.queryData.pageNum=res.data.pageNum;
+					this.queryData.totalPage=res.data.total;
+					this.queryData.pageSize=res.data.pageSize;
+				});
+			},
+			formateTableData(list) {
+				list = list || [];
+				list.forEach(item => {
+					item['projectName'] = '235国道杭州至诸暨公路萧山河上至诸暨安华段改建工程';
+					item['buildUnit'] = '中交上海航道局有限公司、中国交通建设股份有限公司、浙江诸安建设集团有限公司、浙江省交通规划设计研究院有限公司';
+					item['contractCode'] = '235SJSG01';
+					item['supervisorUnit'] = '浙江交科公路水运工程监理有限公司';
+					item['buildSectionId'] = '';
+					item['supervisorSection'] = '';
+					item['statusStr'] = '';
+				})
+				return list;
+			},
+			addNew() {
+				this.editRow=null;
+				this.$refs.edit.changeVisible(true);
+			},
+			modify(row) {
+				this.editRow=row;
+				this.$refs.edit.changeVisible(true);
+			},
+			viewDetail(row) {
+				this.detailRow=row;
+				this.$refs.detail.changeVisible(true);
+			},
+			deleteRow(row) {
+				this.$confirm('确认是否删除?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					api.deleteHiddenProject({id:row['id']}).then((res) => {
+						this.query();
+						this.$message({
+							type: 'success',
+							message: '删除成功!'
+						});
+					}).catch(error => {
+						this.$message({
+							type: 'fail',
+							message: '删除失败!'
+						});
+					});
+				});
+			},
+			handleSizeChange(val) {
+				console.log(`每页 ${val} 条`);
+			},
+			handleCurrentChange(val) {
+				console.log(`当前页: ${val}`);
+			}
+		},
+	};
 </script>
-
-<style lang='scss' scoped>
-
+<style scoped lang="scss">
+	@import "../../assets/css/table.scss"
 </style>
---
+++ src/views/contractManagement/equipmentExitForInspection/edit.vue
@@ -0,0 +1,343 @@
+<template>
+	<div>
+		<el-dialog class="full-dialog defined-dialog" :fullscreen="true" :visible.sync="dialogFormVisible">
+			<template slot="title">
+				{{dialogTitle}}
+				<div class="logo-icon"></div>
+			</template>
+			<el-container>
+				<el-main
+					style="background-color: rgba(0,0 0,0.5);height: calc(100vh - 96px); overflow-y: scroll;padding: 0px;margin: 0;">
+					<div class="form-bg">
+						<div class="form-content">
+							<el-form :model="formData" :rules="rules" ref="ruleForm" label-width="80px">
+								<div class="form-title">
+									<div class="title-big-bar"></div>
+									<strong>退场设备报验单-浙公路（JL）011</strong>
+									<div class="form-btns">
+										<el-button size="medium">暂存</el-button>
+										<el-button size="medium">保存草稿</el-button>
+										<el-button size="medium">选择草稿</el-button>
+										<el-button size="medium" type="primary">复制填充</el-button>
+									</div>
+								</div>
+
+								<div class="form-block">
+									<div class="form-block-title">
+										<div class="title-bar"></div><strong>基本信息</strong>
+									</div>
+									<div class="block-line">
+										<div class="block-item">
+											<div class="block-item-label">项目名称</div>
+											<div class="block-item-value">
+												{{baseInfo.buildSectionName}}
+											</div>
+										</div>
+										<div class="block-item">
+											<div class="block-item-label">合同号</div>
+											<div class="block-item-value">
+												{{baseInfo.contractCode}}
+											</div>
+										</div>
+									</div>
+									<div class="block-line">
+										<div class="block-item">
+											<div class="block-item-label">施工单位</div>
+											<div class="block-item-value">
+												{{baseInfo.buildCompany}}
+											</div>
+										</div>
+										<div class="block-item">
+											<div class="block-item-label">监理单位</div>
+											<div class="block-item-value">
+												{{baseInfo.supervisionUnit}}
+											</div>
+										</div>
+									</div>
+									<div class="block-line">
+
+										<div class="block-item">
+											<div class="block-item-label">工程编号</div>
+											<div class="block-item-value">
+												<el-input v-model="formData.projectCode"></el-input>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="form-block">
+									<div class="form-block-title">
+										<div class="title-bar"></div><strong>退场设备信息</strong>
+									</div>
+									<div class="block-line">
+										
+										<div class="block-item">
+											<div class="block-item-label">监理办<i class="require-icon"></i></div>
+											<div class="block-item-value">
+												<el-form-item prop="unit">
+													<el-input v-model="formData.unit"></el-input>
+												</el-form-item>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="form-block">
+									<div class="form-block-title">
+										<div class="title-bar"></div><strong>附件清单</strong>
+										<span style="font-size: 12px;margin-left: 40px;">支持上传jpg jpeg png mp4 docx doc
+											xisx xis pdf文件，且不超过100m</span>
+									</div>
+
+									<div class="block-line">
+										<upload @afterUp="afterUp($event)"></upload>
+										<!-- <div class="block-table-title">附件</div> -->
+										<div class="block-table-btns">
+											<el-button size="small" type="primary">下载全部</el-button>
+										</div>
+									</div>
+									<div class="block-table">
+										<el-table :data="attachTable" style="width: 100%" border class="have_scrolling">
+											<el-table-column type="index" width="50" align="center" label="序号">
+											</el-table-column>
+											<el-table-column prop="fileName" align="center" label="附件"
+												show-overflow-tooltip>
+											</el-table-column>
+											<el-table-column prop="createTime" width="160px" align="center"
+												label="上传日期">
+											</el-table-column>
+											<el-table-column prop="creatorName" width="120px" align="center"
+												label="上传人">
+											</el-table-column>
+											<el-table-column fixed="right" width="120" align="center" label="操作">
+												<template slot-scope="{ row, $index }">
+													<el-button type="text" size="mini">下载</el-button>
+													<el-button type="text" size="mini">预览</el-button>
+													<el-button type="text" size="mini" @click="deleteAttach(row, $index)">删除</el-button>
+												</template>
+											</el-table-column>
+										</el-table>
+									</div>
+								</div>
+
+								
+								<div class="form-block">
+									<div class="form-block-title">
+										<div class="title-bar"></div><strong>待审批人</strong>
+									</div>
+									<div class="block-line">
+										<div class="block-item">
+											<div class="block-item-label">专业监理工程师<i class="require-icon"></i></div>
+											<div class="block-item-value">
+												<el-form-item prop="qualityCheckUser">
+													<el-select v-model="formData.qualityCheckUser" placeholder="请选择">
+														<el-option v-for="item in userOptions" :key="item.value"
+															:label="item.label" :value="item.value">
+														</el-option>
+													</el-select>
+												</el-form-item>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="form-block">
+									<el-button @click="addOrModify" class="submit-btn" size="small" type="primary">提交
+									</el-button>
+								</div>
+							</el-form>
+						</div>
+					</div>
+				</el-main>
+				<!-- <el-aside width="8px" class="close-wrapper">
+					<div class="close-wrap">
+						<i class="el-icon-caret-right"></i>
+					</div>
+				</el-aside>
+				<el-aside
+					style="width: 410px;background-color: rgb(242, 242, 242);overflow: scroll;height: calc(100vh - 96px);">
+					<tasklog></tasklog>
+				</el-aside> -->
+			</el-container>
+		</el-dialog>
+	</div>
+</template>
+
+<script>
+	import * as api from "@/api/quality";
+	import {
+		formatDateTime,
+		createProjectInfo
+	} from "@/utils/format.js";
+	import upload from "../../common/upload.vue"
+	
+	export default {
+		props: ['editRow'],
+		data() {
+			return {
+				dialogTitle: '项目全生命周期数字管理平台',
+				dialogFormVisible: false,
+				annexTableData: [],
+				activeName: 'first',
+				waitTableData: [],
+				userOptions: [{
+					label: '陈武林',
+					value: 1
+				}],
+				rules: {
+					subProject: [{
+						required: true,
+						message: '请填写分项工程',
+						trigger: 'blur'
+					}],
+					unit: [{
+						required: true,
+						message: '请填写	单位、分部工程',
+						trigger: 'blur'
+					}],
+					hiddenProject: [{
+						required: true,
+						message: '请填写隐蔽工程项目',
+						trigger: 'blur'
+					}],
+					buildCheckselfResult: [{
+						required: true,
+						message: '请填写施工自检结果',
+						trigger: 'blur'
+					}],
+					qualityCheckUser: [{
+						required: true,
+						message: '请选择项目质检负责人',
+						trigger: 'blur'
+					}],
+					projectBuildUser: [{
+						required: true,
+						message: '请选择项目施工负责人',
+						trigger: 'blur'
+					}]
+				},
+				baseInfo: {
+					buildSection: 1,
+					buildSectionName: '',
+					contractCode: '',
+					buildCompany: '',
+					supervisionUnit: '',
+				},
+				formData: { //表单参数
+					attachment: [],
+					buildCheckselfResult: '',
+					deletedFlag: 1,
+					draftFlag: 1,
+					hiddenProject: '',
+					// id: null,
+					projectBuildUser: 1,
+					projectChargeUser: 1,
+					projectCode: '',
+					projectId: this.$store.getters.project['id'],
+					qualityCheckUser: 1,
+					subProject: '',
+					supervisorEngineerUser: 1,
+					supervisorUser: 1,
+					unit: ''
+				},
+				attachTable: [], //附件
+				fileList:[]
+			};
+		},
+		created() {},
+		components: {
+			upload
+		},
+		computed: {},
+		mounted() {
+			this.getProjectInfoById();
+		},
+		watch: {
+			editRow(obj) {
+				if (obj['id']) {
+					this.getDetail(obj['id']);
+				} else {
+					this.formData = {
+						attachment: [],
+						buildCheckselfResult: '',
+						deletedFlag: 1,
+						draftFlag: 1,
+						hiddenProject: '',
+						// id: null,
+						projectBuildUser: 1,
+						projectChargeUser: 1,
+						projectCode: '',
+						projectId: this.$store.getters.project['id'],
+						qualityCheckUser: 1,
+						subProject: '',
+						supervisorEngineerUser: 1,
+						supervisorUser: 1,
+						unit: ''
+					}
+				}
+			}
+		},
+		methods: {
+			changeVisible(value) {
+				this.dialogFormVisible = value;
+			},
+			getProjectInfoById(){
+				api.getProjectInfoById({
+					projectid:this.$store.getters.project['id']
+				}).then((res) => {
+					let data = res['data'] || {};
+					this.baseInfo['buildSectionName']=data['project']?data['project']['name']:'';
+					let list=data['companys'] || [];
+					let info=createProjectInfo(list);
+					this.baseInfo['buildCompany']=info['buildCompany'];
+					this.baseInfo['supervisionUnit']=info['supervisionUnit'];
+				});
+			},
+			getDetail(id) {
+				api.getHiddenProjectDetail({
+					id: id
+				}).then((res) => {
+					let data = res['data'] || {};
+					this.formData = data;
+					this.attachTable=data.attachment||[];
+				});
+			},
+			addOrModify() {
+				this.$refs['ruleForm'].validate((valid) => {
+					if(valid){
+						this.formData.attachment=this.attachTable;
+						api.addOrUpdateHiddenProject(this.formData).then((res) => {
+							if (res.data) {
+								this.$message({
+									type: 'success',
+									message: '提交成功!'
+								});
+								this.dialogFormVisible = false;
+								this.$emit("query");
+							}
+						});
+					}
+					
+				})
+			},
+			afterUp(data){
+				this.attachTable.push({
+					createTime:formatDateTime(data['uploadTime']),
+					fileName:data['fileName'],
+					fileUrl:data['fileId'],
+					creatorName:'test'
+				})
+			},
+			deleteAttach(row, index){
+				this.$confirm('确认是否删除?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					this.attachTable.splice(index,1);
+				});
+			}
+		},
+	};
+</script>
+
+<style scoped lang="scss">
+	@import "../../../assets/css/dialog.scss"
+</style>
