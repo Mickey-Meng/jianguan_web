26a9be8d8b0e55c46d7dfe1aac6d712103346417
gaoxinlong <54693850@qq.com>
2023-04-01 19:24:20 +0800

    新增计量台账页面

---
+++ src/views/metrology/metrology.vue
@@ -0,0 +1,183 @@
+<!--
+ * @Descripttion:劳务分包合同
+ * @version:
+ * @Author: WangHarry
+ * @Date: 2022-05-09 14:10:50
+ * @LastEditors: yangtao
+ * @LastEditTime: 2022-05-11 14:10:57
+-->
+<template>
+	<el-container class="container-box">
+		<el-header>
+			<div class="input-box">
+				<div class="input-value">
+					<el-input v-model="queryData.buildSectionName" placeholder="标段"></el-input>
+				</div>
+			</div>
+			<div class="input-box">
+				<div class="input-value">
+					<el-input v-model="queryData.laborContractProjectName" placeholder="拟劳务合作工程名称"></el-input>
+				</div>
+			</div>
+			<el-button type="primary" @click="query">搜索</el-button>
+			<div v-if="!isDraft" class="right-btns">
+				<div class="operate-btns" v-show="operateBtnsVisible">
+					<el-button size="small" @click="addNew">新增</el-button>
+					<el-button size="small" @click="exportData">导出</el-button>
+					<el-button size="small">批量操作</el-button>
+				</div>
+			</div>
+		</el-header>
+		<el-main>
+			<div class="container">
+				<el-table :data="tableData" style="width: 100%" border height="calc(100% - 48px)"
+					class="have_scrolling">
+					<el-table-column type="index" width="50" align="center" label="序号3">
+					</el-table-column>
+					<el-table-column prop="buildSectionName" align="center" label="施工标段" show-overflow-tooltip>
+					</el-table-column>
+                    <el-table-column prop="contractCode" align="center" label="合同编号" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="laborContractProjectName" align="center" label="拟劳务合作工程名称" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="startDate" align="center" label="备案日期" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column prop="contractUser" align="center" label="承包人" show-overflow-tooltip>
+					</el-table-column>
+					<el-table-column fixed="right" width="120" align="center" label="操作">
+						<template slot-scope="{ row, $index }">
+							<el-button v-if="!isDraft"  type="text" size="mini" @click="modify(row)">修改</el-button>
+							<el-button v-if="!isDraft"  type="text" size="mini" @click="viewDetail(row)">详情</el-button>
+
+							<el-button v-if="isDraft" type="text" size="mini" @click="checkDetail(row)">选择</el-button>
+
+							<el-button  type="text" size="mini" @click="deleteRow(row)">删除</el-button>
+						</template>
+					</el-table-column>
+				</el-table>
+				<el-pagination @current-change="handleCurrentChange" :current-page="queryData.pageNum"
+					:page-size="queryData.pageSize" layout="total, prev, pager, next, jumper"
+					:total="queryData.totalPage">
+				</el-pagination>
+			</div>
+		</el-main>
+
+		<edit ref="edit" @query="query" :editRow="editRow"></edit>
+		<detail ref="detail" :detailRow="detailRow"></detail>
+	</el-container>
+</template>
+
+<script>
+	import * as api from "@/api/contract.js";
+	import edit from './edit.vue';
+	import detail from './detail';
+
+	export default {
+		props:{
+			isDraft:{
+				type:Boolean,
+				default:false
+			}
+		},
+		components: {
+			edit,
+			detail
+		},
+		data() {
+			return {
+				allData: [],
+				tableData: [],
+				operateBtnsVisible: true,
+				queryData: { //查询参数
+					buildSectionName: '',
+					laborContractProjectName: '',
+					pageNum: 1,
+					totalPage: 1,
+					pageSize: 10,
+					buildSection: this.$store.getters.project.id,projectId:this.$store.getters.project['parentid']
+				},
+				editRow: null,
+				detailRow: null
+			};
+		},
+		created() {},
+		computed: {
+		},
+		mounted() {
+			this.query();
+		},
+		methods: {
+			query() {
+				this.queryData.draftFlag=this.isDraft?0:1;
+				api.getContractLaborList(this.queryData).then((res) => {
+					this.allData = res.data || {};
+					this.tableData = this.allData['list']||[];
+					this.queryData.pageNum = res.data.pageNum;
+					this.queryData.totalPage = res.data.total;
+					this.queryData.pageSize = res.data.pageSize;
+				});
+			},
+			addNew() {
+				// this.editRow = null;
+				this.$refs.edit.changeVisible(null,true);
+			},
+			modify(row) {
+				// this.editRow = row;
+				this.$refs.edit.changeVisible(row,true);
+			},
+			viewDetail(row) {
+				this.detailRow = row;
+				this.$refs.detail.changeVisible(true);
+			},
+			deleteRow(row) {
+				this.$confirm('确认是否删除?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					type: 'warning'
+				}).then(() => {
+					api.deleteContractLabor(row['id']).then((res) => {
+						if (this.tableData.length == 1) {
+							this.queryData.pageNum = this.queryData.pageNum> 1 ? this.queryData.pageNum - 1 : 1
+						}
+						this.query();
+						this.$message({
+							type: 'success',
+							message: '删除成功!'
+						});
+					}).catch(error => {
+						this.$message({
+							type: 'fail',
+							message: '删除失败!'
+						});
+					});
+				});
+			},
+			handleCurrentChange(page) {
+				this.queryData.pageNum=page
+				this.query()
+			},
+			checkDetail(row){
+				this.$emit("hideDraft");
+				this.$emit("getDetail",row['id']);
+			},
+			exportData() {
+				this.queryData.draftFlag = 1;
+				api.exportContractLaborList(this.queryData).then((res) => {
+					const reader = new FileReader();
+					reader.readAsDataURL(res);
+					reader.onload = (e) => {
+						const a = document.createElement('a');
+						a.download = `劳务分包合同清单.xls`;
+						a.href = e.target.result;
+						document.body.appendChild(a);
+						a.click();
+						document.body.removeChild(a);
+					};
+				});
+			}
+		},
+	};
+</script>
+<style scoped lang="scss">
+	@import "../../assets/css/table.scss"
+</style>
---
+++ src/views/metrology/view.vue
@@ -0,0 +1,181 @@
+<template>
+	<div>
+
+		<div class="form-content">
+			<el-form ref="form" label-width="80px">
+
+				<div class="form-block">
+					<div class="form-block-title">
+						<div class="title-bar"></div><strong>基本信息</strong>
+					</div>
+					<projectinfo></projectinfo>
+					<!-- <div class="block-line">
+						<div class="block-item">
+							<div class="block-item-label">发起人</div>
+							<div class="block-item-value">
+								{{baseInfo.startupUser}}
+
+							</div>
+						</div>
+					</div> -->
+					<div class="block-line">
+						<div class="block-item">
+							<div class="block-item-label">发起日期</div>
+							<div class="block-item-value">
+								{{formData.startDate}}
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="form-block">
+					<div class="form-block-title">
+						<div class="title-bar"></div><strong>合同信息</strong>
+					</div>
+					<div class="block-line">
+						<div class="block-item">
+							<div class="block-item-label">合同编号</div>
+							<div class="block-item-value">
+								{{formData.contractCode}}
+
+							</div>
+						</div>
+						<div class="block-item">
+							<div class="block-item-label">承包人</div>
+							<div class="block-item-value">
+								{{formData.contractUser}}
+							</div>
+						</div>
+					</div>
+					<div class="form-block-title">
+						<div class="title-bar"></div><strong>合同附件</strong>
+						<span style="font-size: 12px;margin-left: 40px;">支持上传jpg jpeg png mp4 docx doc
+							xisx xis pdf文件，且不超过100m</span>
+					</div>
+
+					<attachlist :editAble="false" ref="attachlist" :attachTable="attachTable"></attachlist>
+
+				</div>
+				<div class="form-block">
+					<div class="form-block-title">
+						<div class="title-bar"></div><strong>信息填报</strong>
+					</div>
+					<div class="block-table">
+						<el-table :data="contractTable" style="width: 100%" border
+							class="have_scrolling">
+							<el-table-column type="index" width="50" align="center" label="序号">
+							</el-table-column>
+							<el-table-column prop="buildProjectName" align="center" label="拟分包工程名称"
+								show-overflow-tooltip>
+							</el-table-column>
+							<el-table-column prop="buildProjectPartName" width="180px" align="center"
+								label="拟分包工程部位">
+							</el-table-column>
+							<el-table-column prop="contractUserName" width="120px" align="center"
+								label="承包人名称">
+							</el-table-column>
+							<el-table-column prop="contractChargeUserName" width="120px" align="center"
+								label="承包负责人">
+							</el-table-column>
+							<el-table-column prop="cardNum" width="120px" align="center" label="身份证号">
+							</el-table-column>
+							<el-table-column prop="contractNum" width="120px" align="center"
+								label="拟分包工程合同金额(元)">
+							</el-table-column>
+							<el-table-column prop="buildStartMonth" width="120px" align="center"
+								label="拟分包施工工期结束月份">
+							</el-table-column>
+							<el-table-column prop="buildEndMonth" width="120px" align="center"
+								label="拟分包施工工期开始月份">
+							</el-table-column>
+							<el-table-column prop="contractUserQualification" width="120px"
+								align="center" label="承包人专业分包资质">
+							</el-table-column>
+							<el-table-column prop="remark" width="120px" align="center" label="备注">
+							</el-table-column>
+
+						</el-table>
+					</div>
+				</div>
+
+
+			</el-form>
+		</div>
+	</div>
+</template>
+
+<script>
+	import * as api from "@/api/contract";
+	import { getUserInfo } from "@/api/user";
+	import attachlist from "../common/attachlist"
+	import projectinfo from "../common/projectinfo.vue"
+
+	import {
+		formatMonth,
+		formatDate,
+		formatDateTime,
+		convertOptions
+	} from "@/utils/format.js";
+
+	export default {
+		data() {
+			return {
+				dialogTitle: '全生命周期智慧建设管理平台',
+				dialogFormVisible: false,
+
+				baseInfo: {
+					buildSection: 1,
+					projectName:'池州市平天湖东部区域棚户区改造建设工程EPC总承包',
+					buildSectionName: '235国道项目部',
+					contractCode: 'ORG_00004',
+					startupUser: '赵赞文',
+					startupDate: formatMonth(new Date()),
+				},
+				formData: { //表单参数
+					attachment: [],
+					contractCode: '',
+					information: [],
+					contractUser: '',
+					deletedFlag: 1,
+					draftFlag: 1,
+					buildSection: this.$store.getters.project.id,
+					projectId:this.$store.getters.project['parentid'],
+					startDate:formatDate(new Date())
+				},
+				attachTable: [], //附件
+				contractTable: []
+			};
+		},
+		created() {},
+		components: {
+			attachlist,
+			projectinfo
+		},
+		computed: {},
+		watch: {
+
+		},
+		mounted() {
+
+		},
+		methods: {
+			getUserInfo(id) {
+				getUserInfo(id).then(res => {
+					this.baseInfo.startupUser = res.data.userInfo.NAME;
+				});
+			},
+			getDetail(id) {
+				api.getContractLaborDeatil(id).then((res) => {
+					let data = res['data'] || {};
+					this.formData = data;
+					this.attachTable = data.attachment || [];
+					this.contractTable = data.information || [];
+					this.getUserInfo(data.createUserId)
+				});
+			},
+		},
+	};
+</script>
+
+<style scoped lang="scss">
+	@import "../../assets/css/dialog.scss"
+</style>
