82fe005f0bdc5a094c21d2ec338527270a3e33ed
gaoxinlong <54693850@qq.com>
2023-04-06 09:22:16 +0800

    驳回状态，可修改和删除数据

--- src/views/metrology/metrology.vue
+++ src/views/metrology/metrology.vue
@@ -68,9 +68,9 @@
           </el-table-column>
 					<el-table-column fixed="right" width="120" align="center" label="操作">
 						<template slot-scope="{ row, $index }">
-							<el-button v-if="row.auditStatus == -1"  type="text" size="mini" @click="modify(row)">修改</el-button>
+							<el-button v-if="row.auditStatus == 'reject'"  type="text" size="mini" @click="modify(row)">修改</el-button>
 							<el-button type="text" size="mini" @click="viewDetail(row)">详情</el-button>
-							<el-button v-if="row.auditStatus == -1" type="text" size="mini" @click="deleteRow(row)">删除</el-button>
+							<el-button v-if="row.auditStatus == 'reject'" type="text" size="mini" @click="deleteRow(row)">删除</el-button>
 						</template>
 					</el-table-column>
 				</el-table>
