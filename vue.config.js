d8fc1ebf685621c59d52fd6cd8e2dc152cf0652b
Mickey <329623516@qq.com>
2023-04-25 23:30:03 +0800

    20230425修改提交

--- vue.config.js
+++ vue.config.js
@@ -34,6 +34,8 @@ module.exports = {
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
   // webpack-dev-server 相关配置
+
+
   devServer: {
     host: '0.0.0.0',
     port: port,
@@ -58,6 +60,9 @@ module.exports = {
     },
     disableHostCheck: true
   },
+
+
+
   configureWebpack: {
     name: name,
     devtool: "eval-source-map", // Debugger for Chrome配置项
